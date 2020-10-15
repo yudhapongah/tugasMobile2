4fc76a6ec0fbb715a2908b8434ca17efbeab411b
yudhapongah <yudhaohmpong@gmail.com>
2020-10-15 20:23:55 +0700

    first commit

---
+++ app/src/main/java/com/example/myintentapp/MoveWithObjectActivity.java
@@ -0,0 +1,20 @@
+package com.example.myintentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MoveWithObjectActivity extends AppCompatActivity {
+    public static String EXTRA_PERSON ="extra_person";
+    private TextView tvObject;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_move_with_object);
+        tvObject = (TextView)findViewById(R.id.tv_object_received);
+        Person mPerson = getIntent().getParcelableExtra(EXTRA_PERSON);
+        String text = "Name : "+mPerson.getName()+", Email : "+mPerson.getEmail()+", Age : "+ mPerson.getCity();
+        tvObject.setText(text);
+    }
+}
